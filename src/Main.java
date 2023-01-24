3bd37863cc738038f84d5cc4a3844e8bca396db0
NAND PATEL <>
2023-01-24 17:25:00 +0000

    added print statement

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
 import java.awt.*;
 import javax.swing.*;
-public class Main{
-    public static void main(String[] args){
 
-        JFrame myFrame = new JFrame();
-        myFrame.setSize(700,500);
+public class Main {
+  public static void main(String[] args) {
 
+    System.out.println("I am on REplit)");
 
+    JFrame myFrame = new JFrame();
+    myFrame.setSize(700, 500);
 
-        BallPanel yourPanel = new BallPanel();
-        yourPanel.setSize(700,500);
+    BallPanel yourPanel = new BallPanel();
+    yourPanel.setSize(700, 500);
 
+    // myFrame.add(myPanel);
+    myFrame.add(yourPanel);
+    myFrame.setVisible(true);
 
-
-
-
-        //myFrame.add(myPanel);
-        myFrame.add(yourPanel);
-        myFrame.setVisible(true);
-
-
-
-
-    }
+  }
 }
