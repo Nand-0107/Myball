0c0315826aab2c1a70c4113cae89d45948e668a3
nand.patel23 <nandpat0107@gmail.com>
2023-02-15 11:24:54 -0600

    i tried 2

--- src/Ball.java
+++ src/Ball.java
@@ -12,15 +12,21 @@ public class Ball{
 
 
     public Ball(){
-        x = (int)(Math.random()*680);
-        y = (int)(Math.random()*480);
-        xSpeed = (int)(Math.random()*20);
-        ySpeed =(int)(Math.random()*20);
-        size = (int)(Math.random()*70);
+
+        x = (int)(Math.random()*680)+1;
+        y = (int)(Math.random()*480)+1;
+        xSpeed = (int)((Math.random()*10));
+        ySpeed = (int)((Math.random()*10));
+        size = (int)((Math.random()+1)*50);
+        color = (new Color((int)((Math.random()*255)),(int)((Math.random()*255)),(int)((Math.random()*255))));
+
     }
 
     public void drawBall(Graphics bob){
+        bob.setColor(color);
         bob.fillOval(x,y,size, size);
+
+
 
     }
 
@@ -34,19 +40,19 @@ public class Ball{
 //
 //    }
 
-//    public void moveRight(){
-//        x++;
-//    }
-//    public void moveLeft(){
-//        x--;
-//    }
-//
-//    public void moveUp(){
-//        y--;
-//    }
-//    public void moveDown(){
-//        y++;
-//    }
+    public void moveRight(){
+        x++;
+    }
+    public void moveLeft(){
+        x--;
+    }
+
+    public void moveUp(){
+        y--;
+    }
+    public void moveDown(){
+        y++;
+    }
 
     public void checkBoundry(JPanel panel2){
         if(x>=panel2.getWidth()-size||x<0){
--- src/BallPanel.java
+++ src/BallPanel.java
@@ -1,85 +1,72 @@
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.util.*;
 import javax.swing.*;
 public class BallPanel extends JPanel{
-//    int x = 100;
-//    int y = 50;
-//    int xSpeed = 1;
-//    int ySpeed = 1;
 
-    Ball noel;
+
+    Ball noel = new Ball();
+    ArrayList<Ball> bal = new ArrayList<>();
 
     public BallPanel(){
-      noel = new Ball();
-        setBackground(new Color(215,12,82));
+
+
+
+          for(int i =0; i<20; i++){
+              bal.add(new Ball());
+          }
+
+
+        setBackground(new Color(24,255,82));
         setFocusable(true);
 
-        addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
+//        addKeyListener(new KeyListener() {
+//            @Override
+//            public void keyTyped(KeyEvent e) {
+//
+//            }
 
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                System.out.println(e.getKeyCode());
-                int key = e.getKeyCode();
-                if(key == 68||key==39){
-                    noel.moveRight();
-                }
-                if(key == 65||key==37){
-                    noel.moveLeft();
-                }
-                if(key == 87||key==38){
-                    noel.moveUp();
-                }
-                if(key == 83||key==40){
-                    noel.moveDown();
-                }
-
-
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
-        });
+//            @Override
+//            public void keyPressed(KeyEvent e) {
+//                System.out.println(e.getKeyCode());
+//                int key = e.getKeyCode();
+//                if(key == 68||key==39){
+//                    noel.moveRight();
+//                }
+//                if(key == 65||key==37){
+//                    noel.moveLeft();
+//                }
+//                if(key == 87||key==38){
+//                    noel.moveUp();
+//                }
+//                if(key == 83||key==40){
+//                    noel.moveDown();
+//                }
+//
+//
+//            }
+//
+//            @Override
+//            public void keyReleased(KeyEvent e) {
+//
+//            }
+//        });
 
 
     }
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-
-//        g.fillOval(x,y,20,20);
-//        x= x + xSpeed;
-//        y= y + ySpeed;
-//
-//        if(x >= getWidth()-20|| x <= 0){
-//            xSpeed=xSpeed*-1;
-//
-//        }
-//        if(y >= getHeight()-20|| y <= 0) {
-//            ySpeed=ySpeed*-1;
-//
-//        }
-        try{
-            Thread.sleep(2);
+        for(Ball ball: bal){
+            ball.drawBall(g);
+            ball.moveBall();
+            ball.checkBoundry(this);
         }
-        catch(Exception e){
-            System.out.println(e);
-        }
-
-
-
-
-            noel.drawBall(g);
-            noel.moveBall();
-            noel.checkBoundry(this);
-
-
+            try{
+                Thread.sleep(3);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
         repaint();
     }
 
